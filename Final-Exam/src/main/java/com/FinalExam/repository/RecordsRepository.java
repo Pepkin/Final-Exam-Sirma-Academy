64a243b3b7766d9a55572dad260ed2bda0bc80ec
Pepkin <gospodinov.petko@icloud.com>
2024-09-07 11:57:28 +0300

    Repository Creation
    Created repository interface for every entity

---
+++ Final-Exam/src/main/java/com/FinalExam/repository/RecordsRepository.java
@@ -0,0 +1,9 @@
+package com.FinalExam.repository;
+
+import com.FinalExam.models.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordsRepository extends JpaRepository<Record, Long> {
+}
---
+++ Final-Exam/src/main/java/com/FinalExam/repository/TeamsRepository.java
@@ -0,0 +1,9 @@
+package com.FinalExam.repository;
+
+import com.FinalExam.models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamsRepository extends JpaRepository<Team, Long> {
+}
